4e63da763d29e50bd8fcd6d78fab837bbf90e42b
Hugo Mouto <hmoutol@gmail.com>
2021-04-06 13:58:33 -0300

    Bloco 5 Dia 1

---
+++ bloco_5/dia_1/test.js
@@ -0,0 +1,9 @@
+let array = ['macaco', 'grilo', 'cachorro', 'lobo', 'raposa'];
+
+for (let key in array) {
+  console.log(key)
+}
+console.log('-----------------------')
+for (let key of array) {
+  console.log(key)
+}
