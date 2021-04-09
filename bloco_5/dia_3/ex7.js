80dc8ac1b1b0a619d707d9dd7953e52fbb2af417
Hugo Mouto <hmoutol@gmail.com>
2021-04-08 21:26:44 -0300

    Bloco5_ex7

---
+++ bloco_5/dia_3/ex7.js
@@ -0,0 +1,10 @@
+// Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+let myTasks = document.querySelector('.my-tasks');
+
+function addNewTask(taskString) {
+  let newTask = document.createElement('span');
+  newTask.innerText = taskString;
+  myTasks.appendChild(newTask);
+}
